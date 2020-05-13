311b9509573c95bfb294872405a61d472d283d9d
Mustapha-Mohamed <55873657+Mustapha-Mohamed@users.noreply.github.com>
2020-05-13 04:45:18 +0200

    Add files via upload

---
+++ jour4/job01/index.php
@@ -0,0 +1,15 @@
+
+<form action="action.php" method="get">
+ <p>Votre nom : <input type="text" name="nom" /></p>
+ <p>Votre âge : <input type="text" name="age" /></p>
+ <p><input type="submit" value="OK"></p>
+</form>
+
+<?php 
+
+   var_dump($_GET);
+
+?>
+
+
+
---
+++ jour4/job02/index.php
@@ -0,0 +1,28 @@
+
+<form action="index.php" method="get">
+  <label for="nom"> Nom </label>
+  <input type="text" name="nom"/>
+ 	
+  <label for="prenom"> prenom </label>
+  <input type="text" name="prenom"/>
+	
+  <label for="email"> mail </label>
+  <input type="mail" name="email"/>
+	
+  <input type="submit"/>
+</form>
+
+<?php 
+	echo "<table>
+			<thead>
+			  <th>Argument</th>
+			  <th>Valeur</th>				
+			</thead>
+		  </table>";
+				
+          foreach($_GET as $argt => $value)
+          {
+              echo "<tr><td>".$argt."</td><td>".$value."</td></tr>";
+          }
+              echo "</table>";
+?>
